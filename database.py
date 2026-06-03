f6888cc75732a5908df9b2657ef3f23e5d68c441
emmanuelwambua767 <emmmanuelwambua767@gmail.com>
2026-06-03 14:05:26 +0300

    first commit

---
+++ database.py
@@ -0,0 +1,103 @@
+import psycopg2
+#establishing a connection to a postgres db
+conn=psycopg2.connect(host='localhost',port=5432,user='postgres',password='9467',dbname='myduka')
+ #cursor object
+cur=conn.cursor()
+def get_products():
+    cur.execute('select * from products')
+    products_data=cur.fetchall()
+    return products_data
+
+# cur.execute("insert into products(name,buying_price,selling_price)values('wallet',300,500)")
+# conn.commit()
+# print(products_data)
+
+def get_sales():
+    cur.execute('select * from sales')
+    sales_data=cur.fetchall()
+    return sales_data
+
+def insert_products2(values):
+    cur.execute(f"insert into products(name,buying_price,selling_price)values{values}")
+    conn.commit()
+
+product1=('tissue',35,50)
+product2=('valon',120,150)
+# insert_products2(product1)
+# insert_products2(product2)
+products_data=insert_products2
+print(products_data)
+
+def insert_products2(values):
+    cur.execute(f"insert into products(name,buying_price,selling_price)values(%s,%s,%s)",values)
+    conn.commit()
+
+product3=('charger',200,350)
+# insert_products2(product3)
+products_data=insert_products2
+print(products_data)
+# Q1
+def get_stocks():
+    cur.execute("select * from stock")
+    stocks_data = cur.fetchall()
+    return stocks_data
+
+stocks_data = get_stocks()
+print(stocks_data)
+
+#insert sales() 
+
+def insert_sales(values):
+    cur.execute("insert into sales(pid,quantity)values(%s,%s)",values)
+    conn.commit()
+
+sales1=(4,8)
+sales2=(3,12)
+sales3=(5,20)
+
+
+# insert_sales(sales1)
+# insert_sales(sales2)
+# insert_sales(sales3)
+
+
+sales_data=get_sales()
+print(sales_data)
+
+# -> insert_stock()
+def insert_stock(values):
+    cur.execute("insert into stock(pid,stock_quantity)values(%s,%s)",values)
+    conn.commit()
+
+stock1=(2,16)
+stock2=(5,21)
+stock3=(7,35)
+
+# insert_stock(stock1)
+# insert_stock(stock2)
+# insert_stock(stock3)
+
+stocks_data = get_stocks()
+print(stocks_data)
+
+
+
+
+
+
+
+# sales_per_day
+
+# SELECT DATE(created_at) AS sale_date, SUM(Quantity) AS sales_per_day FROM sales GROUP BY DATE(created_at) ORDER BY sale_date;
+
+# -> profit_per_day
+
+# SELECT DATE(sales.created_at) AS sale_date, SUM((products.selling_price - products.buying_price) * sales.quantity) AS profit_per_day FROM sales JOIN products ON sales.pid = products.id GROUP BY DATE(created_at) ORDER BY sale_date;
+
+# -> sales_per_product
+
+# SELECT name, SUM(sales.quantity) AS sales_per_product FROM sales JOIN products ON sales.pid = products.id GROUP BY name;
+
+# -> profit_per_product
+
+# SELECT name, SUM((products.selling_price - products.buying_price) * sales.quantity) AS profit_per_product FROM sales JOIN products ON sales.pid = products.id GROUP BY name;
